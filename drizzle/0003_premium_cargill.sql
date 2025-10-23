cf846dad1ef10345f9c61e6f4556d88c58098be8
Jeff <jeffmaniacmeyers@gmail.com>
2025-10-23 16:13:41 -0400

    Delete drizzle directory

--- drizzle/0003_premium_cargill.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `accountPlans` ADD `industry` varchar(255) DEFAULT 'Enterprise Video Platform';--> statement-breakpoint
-ALTER TABLE `accountPlans` ADD `salesMethodology` enum('MEDDICC','SPICED','Command of the Message') DEFAULT 'MEDDICC';--> statement-breakpoint
-ALTER TABLE `accountPlans` ADD `isOpportunity` boolean DEFAULT false;--> statement-breakpoint
-ALTER TABLE `accountPlans` ADD `salesMethodologyFields` json;--> statement-breakpoint
-ALTER TABLE `accountPlans` ADD `opportunityDetails` json;
