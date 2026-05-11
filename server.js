d76a8f037718ac6590ecbed2540c578c62bb87b1
Jeff <jeffmaniacmeyers@gmail.com>
2026-05-11 22:13:03 +0000

    Remove dev server artifact

--- server.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(path.join(__dirname)));
-app.listen(5000, () => console.log('Server running on port 5000'));
